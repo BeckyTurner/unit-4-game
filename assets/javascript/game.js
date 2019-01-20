a2e9085b8c6084eb698959df7fc43422bd933ded
rebeccaturner39 <beckyturner3992@gmail.com>
2019-01-20 16:07:50 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,20 @@ var wins = 0;
 var losses = 0;
 var totalScore = 0;
 
+var crystalOne = randomizer(1, 12);
+var crystalTwo = randomizer(1, 12);
+var crystalThree = randomizer(1, 12);
+var crystalFour = randomizer(1, 12);    
+
+
 $("#wins").text("Wins: " + wins);
 $("#losses").text("Losses: " + losses);
 
-newGame();
+startGame();
 
 // Function to start a new game
-function newGame() {
-    totalScore = 0;
-    scoreToMatch = randomizer(19, 120);
-    var crystalOne = randomizer(1, 12);
-    var crystalTwo = randomizer(1, 12);
-    var crystalThree = randomizer(1, 12);
-    var crystalFour = randomizer(1, 12);
-    console.log(scoreToMatch);
-    console.log(crystalOne, crystalTwo, crystalThree, crystalFour);
+function startGame() {
+    newGame();
 
     $("#random-number").text("Score to match: " + scoreToMatch);
     $("#score").text("Your total score is: " + totalScore);
@@ -25,25 +24,54 @@ function newGame() {
         totalScore = totalScore + crystalOne;
         $("#score").text("Your total score is: " + totalScore);
         checker();
+        console.log(totalScore);
+
     })
 
     $("#crystal2").on("click", function () {
         totalScore = totalScore + crystalTwo;
         $("#score").text("Your total score is: " + totalScore);
         checker();
+        console.log(totalScore);
+
     })
 
     $("#crystal3").on("click", function () {
         totalScore = totalScore + crystalThree;
         $("#score").text("Your total score is: " + totalScore);
         checker();
+        console.log(totalScore);
+
     })
 
     $("#crystal4").on("click", function () {
         totalScore = totalScore + crystalFour;
         $("#score").text("Your total score is: " + totalScore);
         checker();
+        console.log(totalScore);
     })
+
+    function newGame() {
+        totalScore = 0;
+        scoreToMatch = randomizer(19, 120);
+        console.log(scoreToMatch);
+        console.log(crystalOne, crystalTwo, crystalThree, crystalFour);    
+    }
+
+    function checker() {
+
+        if (totalScore === scoreToMatch) {
+            alert("You win!!!!");
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            newGame();
+        } else if (totalScore > scoreToMatch) {
+            alert("You lose!!!");
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            newGame();
+        }
+    }
 }
 
 // Function to randomize numbers
@@ -52,18 +80,5 @@ function randomizer(min, max) {
 }
 
 // Function to check for win or loss
-function checker() {
 
-    if (totalScore === scoreToMatch) {
-        alert("You win!!!!");
-        wins++;
-        $("#wins").text("Wins: " + wins);
-        newGame();
-    } else if (totalScore > scoreToMatch) {
-        alert("You lose!!!");
-        losses++;
-        $("#losses").text("Losses: " + losses);
-        newGame();
-    }
-}
 
